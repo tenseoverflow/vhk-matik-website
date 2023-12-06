d27e6059d521140cf20dad584d3d0ed485122a73
Juljus <juljus@gmail.com>
2023-12-06 23:21:58 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -7,6 +7,8 @@ mkdir -p ./compile_dir
 # copy assets folder to dist
 cp -r ./src/_assets ./dist/
 echo "Assets copied to dist"
+# echo contents of dist folder recursively
+ls -R ./dist/
 
 # run prebuild scripts
 node ./prebuild_scripts/prebuild_blog.en.js
