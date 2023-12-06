d16954255db902f488d7cbd157f77e63b53018be
Juljus <juljus@gmail.com>
2023-12-06 23:07:26 +0200

    added _assets folder
    added _assets folder that gets copied into dist on build. also added a logo into the navbar

--- serve.sh
+++ serve.sh
@@ -1,10 +1,15 @@
+#empty dist folder
+rm -rf ./dist/*
+
 # create folders
 mkdir -p ./compile_dir
+
+# copy assets folder to dist
+cp -r ./src/_assets ./dist/
 
 # run prebuild scripts
 node ./prebuild_scripts/prebuild_blog.en.js
 node ./prebuild_scripts/prebuild_blog.et.js
 
 # serve
-rm -rf ./dist/*
 entu-ssg serve config.yaml full
