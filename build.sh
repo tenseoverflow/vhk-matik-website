9225dcc34d815b58d55e8f973382e61b04815516
Juljus <juljus@gmail.com>
2023-12-06 23:38:31 +0200

    _assets --> assets

--- build.sh
+++ build.sh
@@ -5,8 +5,8 @@ rm -rf ./dist/*
 mkdir -p ./compile_dir
 
 # copy assets folder to dist
-mkdir -p ./dist/_assets
-cp -r ./src/_assets ./dist/
+mkdir -p ./dist/assets
+cp -r ./src/assets ./dist/
 
 # run prebuild scripts
 node ./prebuild_scripts/prebuild_blog.en.js
