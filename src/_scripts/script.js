1b735a870dfaa9045f8229ec4405171eacb01fd2
tenseoverflow <contact@hen.ee>
2024-02-29 00:23:10 +0200

    feat: Responsive navbar

---
+++ src/_scripts/script.js
@@ -0,0 +1,8 @@
+function responsiveNav() {
+  var x = document.getElementById("navbar");
+  if (x.className === "navbar") {
+    x.className += " responsive";
+  } else {
+    x.className = "navbar";
+  }
+}
